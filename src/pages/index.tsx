ce7d9c901f14a2eef8ab9a9f87782fafd40b8b48
Ross <ross.lillis@gmail.com>
2022-04-12 19:05:01 +1000

    feat: create boilerplate

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import type { NextPage } from "next";
+
+import styles from "../styles/Home.module.scss";
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <h1>Next.js app!!</h1>
+    </div>
+  );
+};
+
+export default Home;
